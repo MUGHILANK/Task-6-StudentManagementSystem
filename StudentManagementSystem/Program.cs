d0dbbb845efbe9839cfea229e22493edee8c0c4d
MUGHILAN_OFFICIAL <mca211117@smvec.ac.in>
2025-06-18 18:26:54 +0530

    Add project files.

---
+++ StudentManagementSystem/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace StudentManagementSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StudenetManagement myObj = new StudenetManagement();
+
+            while (true)
+            {
+                Console.Write("\nEnter student Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter student Grade: ");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                myObj.addStudent(name, grade);
+
+                Console.Write("\n\nAdd Another data:Press any Key or For Exit:Press \"N\": ");
+                string exitToken = Console.ReadLine().ToUpper();
+                if (exitToken == "N") { break; }
+            }
+
+            myObj.displayStudent();
+
+            //while (true)
+            //{
+            //    Console.WriteLine("\nEnter the student name to remove: ");
+            //    string removeName = Console.ReadLine();
+            //    myObj.removeStudent(removeName);
+            //    Console.Write("\n\nPress D to display OR Press N to exit: ");
+            //    string exitToken1 = Console.ReadLine().ToUpper();  
+            //    if (exitToken1 == "N") { break; } 
+            //    else if(exitToken1 == "D")  { myObj.displayStudent(); Console.WriteLine("After "); }
+            //}
+
+
+            Console.Write("Enter Student name for search: ");
+            string serachName = Console.ReadLine();
+            myObj.SearchStudent(serachName);
+
+
+        }
+         
+
+
+
+    }
+}
---
+++ StudentManagementSystem/StudenetName.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    internal class StudenetManagement
+    {
+        List<string> studentName = new List<string>();
+        Dictionary<string,double> studentGrade = new Dictionary<string,double>();
+
+        public void addStudent(string name,double grade) 
+        { 
+            studentName.Add(name);
+            studentGrade[name] = grade;
+            Console.WriteLine($"Data inserted...!");
+
+        }
+        public void displayStudent() 
+        {
+            Console.WriteLine("\nStudent List");
+            foreach (var name in studentName)
+            {
+                Console.WriteLine($"Name:\t{name},Grade:\t{studentGrade[name]}");
+            }
+        }
+
+        public void removeStudent(string name) 
+        {
+            if (studentName.Contains(name))
+            {
+                studentName.Remove(name);
+                studentGrade.Remove(name);
+                Console.WriteLine($"Student {name} is removed...!");
+            }
+            else { Console.WriteLine("Student Not found....!"); }
+
+        }
+        public void updateStudent(string name,double newgrade) 
+        {
+            if (studentGrade.ContainsKey(name))
+            {
+                studentGrade[name] = newgrade;
+                Console.WriteLine("Student grade is updated!!"); 
+            }
+            else 
+            {
+                Console.WriteLine("Student Not Found!!!");
+            }
+        
+        }
+
+        public void SearchStudent(string keyword)
+        {
+            var result = studentName.Where(n => n.ToLower().Contains(keyword.ToLower()));
+
+            Console.WriteLine("\nSearch Result:");
+            foreach (var name in result)
+            {
+                Console.WriteLine($"Name: {name}, Grade: {studentGrade[name]}");
+            }
+
+            if (!result.Any())
+                Console.WriteLine("No student found.");
+        }
+
+
+    }
+}
